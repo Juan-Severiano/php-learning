9602f67db6bd707593896e424ff8283c390b1b12
Francisco Juan <juansoussev@gmail.com>
2024-07-29 21:57:39 -0300

    first commit

---
+++ ex001/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ServerData</title>
+</head>
+<body>
+  <h1>ServerData</h1>
+  <?php
+    $name = "Francisco Juan";
+    echo "<p>Hello, $name</p>";
+  ?>
+</body>
+</html>
